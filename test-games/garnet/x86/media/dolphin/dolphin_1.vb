9e2fc7329b7f467968211293da29db0b37bc89d7
randomgraphics <gfxrgb@gmail.com>
2025-10-07 10:22:54 -0700

    added x86 test games (#4)

---
+++ test-games/garnet/x86/media/dolphin/dolphin_1.vb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8f4c68f0fd04d9ee40607089d27a6214890645cf65bceb35557ce915baa3579
+size 9120
---
+++ test-games/garnet/x86/media/dolphin/dolphin_2.vb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:76a81bf8143f7d8007434670f6b3dd212c83268efd50412dd4a4696505f50791
+size 9120
